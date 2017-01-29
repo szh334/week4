eaa276b573961cee0c21ffa3a385eb53463fc9fb
szh334 <su.zhu@kellogg.northwestern.edu>
2017-01-29 09:53:20 -0600

    updated todo with comments and cleanup

--- todo/todo.js
+++ todo/todo.js
@@ -2,16 +2,14 @@
 $(function() {
 
   $(".clickdone").on("click", function(event) {
-    event.preventDefault()
     var elementThatWasClicked = $(this)
-    var removedElement = elementThatWasClicked.parents().eq(1)
-    console.log(elementThatWasClicked.parents().eq(1) + "has been removed")
-    //elementThatWasClicked.remove()
-    removedElement.remove()
-    console.log("there are " + $("div").length + "divs remaining")
-    //$.when($( "div" ).length = 0).then(console.log("all done"))
-    if($( "div" ).length <= 0) {console.log("all done!");
-                                $(".allDone").fadeIn()
-                                }
+    var removedElement = elementThatWasClicked.parents().eq(1) //store second level parent to delete the row
+    console.log(elementThatWasClicked.parents().eq(1) + "has been removed")//log which row to remove
+    removedElement.remove()//remove row
+    console.log("there are " + $("div").length + "divs remaining")//check how many list divs are remaining
+    if($( "div" ).length <= 0)//if all divs have been removed, then show the hidden stuff
+      {console.log("all done!");
+        $(".allDone").fadeIn()
+      }
   })
 })
